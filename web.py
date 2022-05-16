afae964c116af0e8f040da79708fd7a9d003ad9b
kaniket7209 <kaniket7209@gmail.com>
2022-05-16 11:24:14 +0530

    Deploy

--- web.py
+++ web.py
@@ -36,5 +36,5 @@ def upload_image():
     
     return render_template('chooseFile.html')
 
-if __name__ == "__main__":
-    app.run(debug=True,port = 7900)
+# if __name__ == "__main__":
+#     app.run(debug=True,port = 7900)
